ed77b969d57e9587b8283f79156b74a606e88761
Andrey Stepachev <octo47@gmail.com>
2013-07-06 01:06:22 +0400

    pro to for internal use and public was separated

--- gen_proto.sh
+++ gen_proto.sh
@@ -1,1 +1,2 @@
-protoc --java_out src/main/java src/main/java/popeye/transport/proto/message.proto
+protoc --proto_path src/main/java/ --java_out src/main/java src/main/java/popeye/transport/proto/message.proto
+protoc --proto_path src/main/java/ --java_out src/main/java src/main/java/popeye/transport/proto/storage.proto
