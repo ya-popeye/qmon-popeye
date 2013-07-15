a2cc36924c28b521cf63e9c60550ad84ca00435c
Andrey Stepachev <octo47@gmail.com>
2013-07-15 10:44:10 +0400

    copy dependencies into target/lib

--- project/Build.scala
+++ project/Build.scala
@@ -1,18 +1,17 @@
 import sbt._
 import sbt.Keys._
 import net.virtualvoid.sbt.graph.{Plugin => Dep}
-import sbtassembly.Plugin._
-import AssemblyKeys._
 
 object Version {
   val Scala = "2.10.1"
-  val Akka = "2.1.4"
-  val Spray = "1.1-M8"
+  val Akka = "2.2.0"
+  val Spray = "1.2-M8"
   val Hadoop = "1.2.0"
   val HBase = "0.94.6"
   val ScalaTest = "1.9.1"
   val Mockito = "1.9.0"
   val Jackson = "1.8.8"
+  val Kafka = "0.8.0-beta1"
 }
 
 
@@ -39,10 +38,6 @@ object PopeyeBuild extends Build {
       Tests.defaultSettings ++
       Dep.graphSettings
 
-  lazy val kafka = ProjectRef(
-    build = uri("git://github.com/resetius/kafka.git#0.8-scala-2.10-stbx"),
-    project = "core")
-
   lazy val popeye = Project(
     id = "popeye",
     base = file("."),
@@ -52,41 +47,29 @@ object PopeyeBuild extends Build {
   lazy val popeyeServer = Project(
     id = "popeye-server",
     base = file("server"),
-    settings = defaultSettings ++ assemblySettings)
-    .dependsOn(kafka)
-    .dependsOn(kafka % "test->test")
+    settings = defaultSettings)
     .settings(
-    projectDependencies ~= {
-      deps => deps map {
-        dep =>
-          dep.excludeAll(
-            ExclusionRule(organization = "log4j"),
-            ExclusionRule(organization = "org.slf4j", name = "slf4j-simple")
-          )
-      }
-    })
-    .settings(
-        libraryDependencies ++= Seq(
-          "com.google.protobuf" % "protobuf-java" % "2.4.1",
-          "com.yammer.metrics" % "metrics-core" % "2.2.0",
-          "com.yammer.metrics" % "metrics-annotation" % "2.2.0",
-          "com.typesafe.akka" %% "akka-kernel" % Version.Akka,
-          "com.typesafe.akka" %% "akka-actor" % Version.Akka,
-          "com.typesafe.akka" %% "akka-agent" % Version.Akka,
-          "com.typesafe.akka" %% "akka-slf4j" % Version.Akka,
-          "io.netty" % "netty" % "3.6.6.Final",
-          "io.spray" % "spray-can" % Version.Spray,
-          "io.spray" % "spray-io" % Version.Spray,
-          "org.codehaus.jackson" % "jackson-core-asl" % Version.Jackson,
-          "org.slf4j" % "jcl-over-slf4j" % "1.7.5",
-          "org.slf4j" % "slf4j-log4j12" % "1.7.5",
-          "org.hbase" % "asynchbase" % "1.4.1"
-            exclude("org.slf4j", "log4j-over-slf4j")
-            exclude("org.slf4j", "jcl-over-slf4j"),
-          "org.scalatest" %% "scalatest"                           % Version.ScalaTest % "test",
-          "org.mockito"    % "mockito-core"                        % Version.Mockito   % "test",
-          "com.typesafe.akka" %% "akka-testkit" % Version.Akka % "test"
-        )
+    managedDirectory := file("target/libs/"),
+    retrieveManaged := true,
+    retrievePattern := "[conf]/[artifact](-[revision])(-[classifier]).[ext]",
+    libraryDependencies ++= Seq(
+      "com.google.protobuf" % "protobuf-java" % "2.4.1",
+      "org.apache.kafka" %% "kafka" % Version.Kafka % "compile->compile;test->test"
+        exclude("org.slf4j", "slf4j-simple"),
+      "com.typesafe.akka" %% "akka-actor" % Version.Akka,
+      "com.typesafe.akka" %% "akka-slf4j" % Version.Akka,
+      "io.spray" % "spray-can" % Version.Spray,
+      "io.spray" % "spray-io" % Version.Spray,
+      "org.codehaus.jackson" % "jackson-core-asl" % Version.Jackson,
+      "org.slf4j" % "jcl-over-slf4j" % "1.7.5",
+      "org.slf4j" % "slf4j-log4j12" % "1.7.5",
+      "org.hbase" % "asynchbase" % "1.4.1"
+        exclude("org.slf4j", "log4j-over-slf4j")
+        exclude("org.slf4j", "jcl-over-slf4j"),
+      "org.scalatest" %% "scalatest" % Version.ScalaTest % "test",
+      "org.mockito" % "mockito-core" % Version.Mockito % "test",
+      "com.typesafe.akka" %% "akka-testkit" % Version.Akka % "test"
     )
+  )
 }
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,10 +1,6 @@
-addSbtPlugin("com.typesafe.akka" % "akka-sbt-plugin" % "2.1.4")
-
 addSbtPlugin("io.spray" % "sbt-revolver" % "0.6.2")
 
 addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.4")
 
 addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.4.0")
-
-addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.9.0")
 
