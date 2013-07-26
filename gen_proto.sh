7161b48167ee9139dc930d6a093123857a1b325e
Andrey Stepachev <octo@yandex-team.ru>
2013-07-27 00:36:24 +0400

    gen_proto.sh fixed for new layout

--- gen_proto.sh
+++ gen_proto.sh
@@ -1,2 +1,2 @@
-protoc --proto_path server/src/main/java/ --java_out server/src/main/java server/src/main/java/popeye/transport/proto/message.proto
-protoc --proto_path server/src/main/java/ --java_out server/src/main/java server/src/main/java/popeye/transport/proto/storage.proto
+protoc --proto_path core/src/main/java/ --java_out core/src/main/java core/src/main/java/popeye/transport/proto/message.proto
+protoc --proto_path core/src/main/java/ --java_out core/src/main/java core/src/main/java/popeye/transport/proto/storage.proto
