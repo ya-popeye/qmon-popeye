8fd80e60ef7a71bb8fcf9ec51be1814acaa8a91c
Andrey Stepachev <octo47@gmail.com>
2013-08-03 19:42:48 +0400

    eliminate one buffer copy

--- core/src/main/scala/popeye/transport/proto/PackedPoints.scala
+++ core/src/main/scala/popeye/transport/proto/PackedPoints.scala
@@ -2,16 +2,18 @@ package popeye.transport.proto
 
 import popeye.transport.proto.Message.Point
 import com.google.protobuf.{CodedInputStream, CodedOutputStream}
-import java.io.{InputStream, ByteArrayOutputStream}
+import java.io.{OutputStream, InputStream, ByteArrayOutputStream}
 import scala.collection.mutable.ArrayBuffer
+import java.util.Arrays
+import java.util
 
 /**
  * @author Andrey Stepachev
  */
-class PackedPoints {
-  private[this] val hashes = new ByteArrayOutputStream()
+class PackedPoints(avgMessageSize: Int = 100, messagesPerExtent: Int = 100) {
+  private[this] val hashes = new ExpandingBuffer(messagesPerExtent * 8)
   private[this] val hashesCoder = CodedOutputStream.newInstance(hashes)
-  private[this] val points = new ByteArrayOutputStream()
+  private[this] val points = new ExpandingBuffer(messagesPerExtent * avgMessageSize)
   private[this] val pointsCoder = CodedOutputStream.newInstance(points)
   var pointsCount = 0
 
@@ -28,12 +30,12 @@ class PackedPoints {
     this
   }
 
-  def pointsBuffer = {
+  private[proto] def pointsBuffer = {
     pointsCoder.flush()
     points.toByteArray
   }
 
-  def hashesBuffer = {
+  private[proto] def hashesBuffer = {
     hashesCoder.flush()
     hashes.toByteArray
   }
@@ -41,24 +43,73 @@ class PackedPoints {
   def asPacketsBuffer = {
     hashesCoder.flush()
     pointsCoder.flush()
-    new PackedPointsBuffer(points.toByteArray, hashes.toByteArray, pointsCount)
+    new PackedPointsBuffer(points, hashes, pointsCount)
+  }
+}
+
+class ExpandingBuffer(extent: Int) extends OutputStream {
+  private[this] var buf = new Array[Byte](extent)
+  private[this] var count = 0;
+
+  def write(b: Int) {
+    val newcount = count + 1
+    if (newcount > buf.length) {
+      buf = util.Arrays.copyOf(buf, Math.max(buf.length + extent, newcount))
+    }
+    buf(count) = b.asInstanceOf[Byte]
+    count = newcount
+  }
+
+  private[proto] def buffer: Array[Byte] = buf
+
+  override def write(b: Array[Byte], off: Int, len: Int) {
+    if ((off < 0) || (off > buf.length) || (len < 0) || ((off + len) > buf.length) || ((off + len) < 0)) {
+      throw new IndexOutOfBoundsException
+    }
+    else if (len == 0) {
+      return
+    }
+    val newcount = count + len
+    if (newcount > buf.length) {
+      buf = util.Arrays.copyOf(buf, Math.max(buf.length + extent, newcount))
+    }
+    System.arraycopy(b, off, buf, count, len)
+    count = newcount
+  }
+
+  def writeTo(out: OutputStream) {
+    out.write(buf, 0, count)
+  }
+
+  def toByteArray: Array[Byte] = {
+    util.Arrays.copyOf(buf, count)
+  }
+
+  def toCodedInputStream: CodedInputStream = {
+    CodedInputStream.newInstance(buf, 0, count)
   }
 }
 
 case class PackedPointsIndex(hash: Int, offset: Int, len: Int, delimitedPoints: Array[Byte])
 
-class PackedPointsBuffer(points: Array[Byte], hashes: Array[Byte], pointsCount: Int) extends Traversable[PackedPointsIndex] {
+class PackedPointsBuffer private[proto] (points: ExpandingBuffer, hashes: ExpandingBuffer, pointsCount: Int) extends Traversable[PackedPointsIndex] {
+
+  override def size: Int = pointsCount
+
+  override def isEmpty: Boolean = {
+    pointsCount == 0
+  }
 
   def foreach[U](f: (PackedPointsIndex) => U) {
-    val meta = CodedInputStream.newInstance(hashes)
-    val data = CodedInputStream.newInstance(points)
+    val meta = hashes.toCodedInputStream
+    val data = points.toCodedInputStream
     var off = 0
     while (!meta.isAtEnd) {
       val hash = meta.readInt32()
       val size = data.readRawVarint32()
       data.skipRawBytes(size) // skip message
       val rawSize = size + CodedOutputStream.computeRawVarint32Size(size)
-      f(PackedPointsIndex(hash, off, rawSize, points))
+      f(PackedPointsIndex(hash, off, rawSize, points.buffer))
       off += rawSize
     }
   }
