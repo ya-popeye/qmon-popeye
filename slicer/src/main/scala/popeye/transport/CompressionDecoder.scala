bd029e87ba1d0d2f50cce6c2f93ce2de48c6d1d1
Andrey Stepachev <octo47@gmail.com>
2013-08-12 20:15:24 +0400

    refactored out Gzip codec

--- slicer/src/main/scala/popeye/transport/CompressionDecoder.scala
+++ slicer/src/main/scala/popeye/transport/CompressionDecoder.scala
@@ -1,30 +1,119 @@
 package popeye.transport
 
-import java.util.zip.{DataFormatException, ZipException, Inflater}
+import java.util.zip.{ZipException, Inflater}
 import akka.util.ByteString
 import scala.annotation.tailrec
 import java.io.Closeable
 import CompressionDecoder._
 
 object CompressionDecoder {
-  sealed trait Codec
-  case class Gzip() extends Codec
-  case class Snappy() extends Codec
+
+  trait Decoder {
+    protected var pushedBack: Option[ByteString] = None
+
+    def pushBack(what: ByteString) = {
+      pushedBack match {
+        case Some(b) => pushedBack = Some(b ++ what)
+        case None => pushedBack = Some(what)
+      }
+    }
+
+    def decompress[U](input: ByteString, f: (ByteString) => U): Unit
+
+    def close(): Unit
+  }
+
+  class InflaterDecoder extends Decoder {
+    protected val outputBuf = new Array[Byte](64 * 1024)
+    private[this] var remaining: Option[ByteString] = None
+    private[this] val inflater = new Inflater()
+
+    def decompress[U](input: ByteString, f: (ByteString) => U): Unit = {
+      if (input.length > 0) {
+        val buffer = if (remaining.isDefined) {
+          (remaining.get ++ input).toArray
+        } else {
+          input.toArray
+        }
+        inflater.setInput(buffer)
+        drain(f)
+
+        if (inflater.getRemaining > 0) {
+          remaining = Some(ByteString.fromArray(
+            buffer,
+            buffer.length - inflater.getRemaining,
+            buffer.length)
+          )
+        }
+        validate()
+      }
+    }
+
+    @tailrec
+    private def drain[U](f: (ByteString) => U): Unit = {
+      val len = inflater.inflate(outputBuf, 0, outputBuf.length)
+      if (len > 0) {
+        val data = ByteString.fromArray(outputBuf, 0, len)
+        val completeData = if (pushedBack.isDefined) {
+          val pb = pushedBack.get
+          pushedBack = None
+          pb ++ data
+        } else {
+          data
+        }
+        f(completeData)
+        drain(f)
+      }
+    }
+
+    def close(): Unit = {
+      if (!inflater.finished())
+        inflater.end()
+    }
+
+    private def validate(): Boolean = if (inflater.needsDictionary)
+      throw new ZipException("ZLIB dictionary missing")
+    else
+      true
+
+  }
+
+  class SnappyDecoder extends Decoder {
+    private[this] var buffered = ByteString.empty
+
+    def close(): Unit = {
+      buffered = ByteString.empty
+    }
+
+    def decompress[U](input: ByteString, f: (ByteString) => U): Unit = {
+      throw new NotImplementedError("not yet")
+    }
+  }
+
+
+  sealed trait Codec {
+    def makeDecoder: Decoder
+  }
+
+  case class Gzip() extends Codec {
+    def makeDecoder: Decoder = new InflaterDecoder
+  }
+
+  case class Snappy() extends Codec {
+    def makeDecoder: Decoder = new SnappyDecoder
+  }
+
 }
 
 /**
  * @author Andrey Stepachev
  */
-class CompressionDecoder(var limit: Int, val codec: Codec = Gzip) extends Closeable {
-
+class CompressionDecoder(var limit: Int, val codec: Codec = Gzip()) extends Closeable {
 
   def isClosed = closed
-  private[this] var closed = false
 
-  private[this] val inflater = new Inflater()
-  private[this] val inputBuf = new Array[Byte](2048)
-  private[this] val outputBuf = new Array[Byte](2048)
-  private[this] var pushedBack: Option[ByteString] = None
+  private[this] var closed = false
+  private[this] val decoder = codec.makeDecoder
 
   /**
    * Return decoded bytes to internal buffer.
@@ -34,10 +123,7 @@ class CompressionDecoder(var limit: Int, val codec: Codec = Gzip) extends Closea
    * @param what what to buffer
    */
   def pushBack(what: ByteString): Unit = {
-    pushedBack match {
-      case Some(b) => pushedBack = Some(b ++ what)
-      case None => pushedBack = Some(what)
-    }
+    decoder.pushBack(what)
   }
 
   @tailrec
@@ -48,7 +134,7 @@ class CompressionDecoder(var limit: Int, val codec: Codec = Gzip) extends Closea
         if (rem.isDefined) {
           // if we got remainder, we finished decoding
           // (due of limit or other causes)
-          // need to return all remaingn input as remainder
+          // need to return all remaining input as remainder
           // thats it, we construct one big ByteString
           require(isClosed)
           val b = ByteString.newBuilder
@@ -78,7 +164,7 @@ class CompressionDecoder(var limit: Int, val codec: Codec = Gzip) extends Closea
       throw new IllegalStateException("Decoder closed")
 
     def traverse(input: ByteString): Unit = {
-      decompress(input, f)
+      decoder.decompress(input, f)
       limit -= input.length // calling parent should ensure, that limit is obeyed
       if (limit == 0) {
         close()
@@ -96,47 +182,10 @@ class CompressionDecoder(var limit: Int, val codec: Codec = Gzip) extends Closea
 
   }
 
-  protected def decompress[U](input: ByteString, f: (ByteString) => U) = {
-    input.asByteBuffers foreach {
-      buffer =>
-        while (buffer.hasRemaining) {
-          var off = 0
-          do {
-            val len = Math.min(inputBuf.length, buffer.remaining())
-            buffer.get(inputBuf, off, len)
-            off += len
-          } while (off < inputBuf.length && buffer.hasRemaining)
-          if (off > 0) {
-            inflater.setInput(inputBuf, 0, off)
-            drain(f)
-            if (inflater.needsDictionary) throw new ZipException("ZLIB dictionary missing")
-          }
-        }
-    }
-  }
-
-  @tailrec
-  private def drain[U](f: (ByteString) => U): Unit = {
-    if (!inflater.finished()) {
-      val len = inflater.inflate(outputBuf)
-      if (len > 0) {
-        val data = ByteString.fromArray(outputBuf, 0, len)
-        val completeData = if (pushedBack.isDefined) {
-          val pb = pushedBack.get
-          pushedBack = None
-          pb ++ data
-        } else {
-          data
-        }
-        f(completeData)
-        drain(f)
-      }
-    }
-  }
-
-  def close() {
-    if (!inflater.finished())
-      inflater.end()
+  def close() = {
+    if (!closed)
+      decoder.close()
     closed = true
   }
+
 }
