52230d30acc6fa87d083d74026646a2b6eb6a47e
Vasily Kolpakov <vasliykolpakov@gmail.com>
2013-11-18 19:43:31 +0400

    bash compatibility in test environment runner

--- test-env/kafka/run.sh
+++ test-env/kafka/run.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-if [ "x$KAFKA_HOME" == "x" ]; then
+if [ "x$KAFKA_HOME" = "x" ]; then
   echo "need KAFKA_HOME"
 fi
 export SCALA_VERSION=2.10
 me=$(pwd)/$(dirname $0)
 num=$1
-echo $num; 
-mkdir -p run/server$num; 
-rm -f run/server$num/*.log; 
-if [ "$num" == "zk" ]; then
+echo $num;
+mkdir -p run/server$num;
+rm -f run/server$num/*.log;
+if [ "$num" = "zk" ]; then
 (cd run/server$num && $KAFKA_HOME/bin/zookeeper-server-start.sh $me/config/zookeeper.properties)
 else
 (cd run/server$num && JMX_PORT=988$num $KAFKA_HOME/bin/kafka-server-start.sh $me/config/server${num}.properties)
