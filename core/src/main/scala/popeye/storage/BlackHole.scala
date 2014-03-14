b4d5db012bb43fc2cfa3b0ef49cb65b488df2141
VasilyKolpakov <vasliykolpakov@gmail.com>
2014-03-14 20:17:19 +0400

    optional batch commit delay in BlackHole sink

--- core/src/main/scala/popeye/storage/BlackHole.scala
+++ core/src/main/scala/popeye/storage/BlackHole.scala
@@ -3,9 +3,9 @@ package popeye.storage
 import popeye.pipeline._
 import com.typesafe.config.Config
 import scala.concurrent.{Promise, Future, ExecutionContext}
-import akka.actor.Props
 import popeye.proto.PackedPoints
 import popeye.Logging
+import scala.concurrent.duration._
 
 /**
  * @author Andrey Stepachev
@@ -16,10 +16,23 @@ class BlackHole {
 
 class BlackHolePipelineSinkFactory extends PipelineSinkFactory with Logging {
   def startSink(sinkName: String, channel: PipelineChannel, config: Config, storagesConfig: Config, ect: ExecutionContext): Unit = {
+    implicit val ectx = ect
+    val delayOption =
+      if (config.hasPath("delay")) Some(config.getMilliseconds("delay"))
+      else None
     channel.startReader(sinkName, new PointsSink {
       def send(batchIds: Seq[Long], points: PackedPoints): Future[Long] = {
         debug(s"Blackhole: ${batchIds.mkString}, ${points.pointsCount} points")
-        Promise.successful(points.pointsCount.toLong).future
+        delayOption match {
+          case Some(delay) =>
+            val p = Promise[Long]()
+            channel.actorSystem.scheduler.scheduleOnce(delay.toInt millisecond, new Runnable {
+              override def run(): Unit = p.success(points.pointsCount.toLong)
+            })
+            p.future
+          case None =>
+            Future.successful(points.pointsCount.toLong)
+        }
       }
     })
   }
