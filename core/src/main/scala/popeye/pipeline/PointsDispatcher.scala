a047229fa9b9b04686c9d3f423838ff44baa3a4d
VasilyKolpakov <vasliykolpakov@gmail.com>
2014-03-16 21:24:04 +0400

    performance metrics for PointsDispatcher

--- core/src/main/scala/popeye/pipeline/PointsDispatcher.scala
+++ core/src/main/scala/popeye/pipeline/PointsDispatcher.scala
@@ -6,6 +6,7 @@ import akka.actor.ActorRef
 import scala.concurrent.Promise
 import com.typesafe.config.Config
 import com.codahale.metrics.MetricRegistry
+import java.util.concurrent.atomic.AtomicInteger
 
 class PointsDispatcherConfig(config: Config) extends DispatcherConfig(config) {
 }
@@ -13,6 +14,17 @@ class PointsDispatcherConfig(config: Config) extends DispatcherConfig(config) {
 class PointsDispatcherMetrics(prefix: String, override val metricRegistry: MetricRegistry)
   extends DispatcherMetrics(prefix, metricRegistry) {
   val pointsMeter = metrics.meter(s"$prefix.points")
+  val batchSize = metrics.histogram(s"$prefix.points-batch-size")
+  val queueSizeAfterFlush = metrics.histogram(s"$prefix.queue-size-after-flush")
+  val queueSizeAfterWorkDone = metrics.histogram(s"$prefix.queue-size-after-work-done")
+  val queueEmpty = metrics.meter(s"$prefix.queue-empty")
+  private val id = new AtomicInteger(0)
+
+  def addQueueGauge(queue: PointsQueue) = {
+    metrics.gauge(s"$prefix.queue-size-${id.getAndIncrement}") {
+      queue.stat.points
+    }
+  }
 }
 
 trait PointsDispatcherWorkerActor extends WorkerActor {
@@ -27,7 +39,11 @@ trait PointsDispatcherActor extends DispatcherActor {
 
   def idGenerator: IdGenerator
 
-  lazy val pendingPoints = new PointsQueue(config.lowWatermark, config.highWatermark)
+  lazy val pendingPoints = {
+    val queue = new PointsQueue(config.lowWatermark, config.highWatermark)
+    metrics.addQueueGauge(queue)
+    queue
+  }
 
   def spawnWorker(): ActorRef
 
@@ -41,7 +57,11 @@ trait PointsDispatcherActor extends DispatcherActor {
   def unbuffer(ignoreMinSize: Boolean): Option[WorkerData] = {
     val batchId = idGenerator.nextId()
     val (data, promises) = pendingPoints.consume(ignoreMinSize)
+    if (pendingPoints.stat.points == 0) {
+      metrics.queueEmpty.mark()
+    }
     if (!data.isEmpty) {
+      metrics.batchSize.update(data.map(_.pointsCount).sum)
       log.debug(s"Sending ${data.foldLeft(0)({
         (a, b) => a + b.bufferLength
       })} bytes, will trigger ${promises.size} promises")
