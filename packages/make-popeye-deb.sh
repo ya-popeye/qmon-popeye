7267dc2a44980a764767ebe30212555fd2f586bc
Andrey Stepachev <octo@yandex-team.ru>
2014-05-27 19:13:53 +0400

    fixing relative paths

--- packages/make-popeye-deb.sh
+++ packages/make-popeye-deb.sh
@@ -8,11 +8,11 @@ home=$(dirname $0)
 group=$(cat $home/../build.sbt | grep organization | awk '{ print $5 }' | tr -d '"' )
 version=$(cat $home/../build.sbt | grep version | awk '{ print $5 }' | tr -d '"' )
 
-sed -i.bak -e "s/@GROUP@/$group/g" -e "s/@VERSION@/$version/g" debian/rules
+sed -i.bak -e "s/@GROUP@/$group/g" -e "s/@VERSION@/$version/g" $home/debian/rules
 $home/debian/make-app.sh slicer
 $home/debian/make-app.sh pump
 $home/debian/make-app.sh query
-(cd $home && rm -f debian/changelog && \
+(cd $home && rm -f $home/debian/changelog && \
 /usr/bin/dch --create \
    --package qmon-popeye \
    --newversion 0.9.0.1 --distribution $(lsb_release -sc) "Autopackage"
