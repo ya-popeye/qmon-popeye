0526148b591cb0236b571b3afa101b229804f434
Andrey Stepachev <octo@yandex-team.ru>
2014-06-02 19:07:28 +0400

    get rid of handcrafted packages

--- packages/qmon-popeye-statbox-kafka-config/scripts/publish-lines-cron.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-(flock -n 9 || exit 0 && /usr/bin/logbroker-locked.sh --command /usr/share/statbox-logbroker/publish-lines.sh --lock "lock-publish-lines") 9>/var/lock/publish-lines.lock
--- packages/qmon-popeye-statbox-kafka-config/scripts/release.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-git pull && dch -i && git commit -a && git tag tag-statbox-logbroker-`dpkg-parsechangelog | grep Version | cut -f 2 -d ' '` && git push && git push --tags && debuild -b
