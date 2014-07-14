c7fd6eee1e05634fda3490b7a285011275af466c
VasilyKolpakov <vasilykolpakov@gmail.com>
2014-07-14 20:04:36 +0400

    Fix: splitter race condition

---
+++ core/src/main/scala/popeye/util/ZkConnect.scala
@@ -0,0 +1,49 @@
+package popeye.util
+
+case class ZkConnect(hostAndPorts: Seq[(String, Option[Int])], chroot: Option[String]) {
+  def toZkConnectString = {
+    chroot.fold(serversString)(chrootStr => s"$serversString$chrootStr")
+  }
+
+  def serversString = {
+    val serverStrings = hostAndPorts.map {
+      case (host, port) => port.fold(host)(portStr => s"$host:$portStr")
+    }
+    serverStrings.mkString(",")
+  }
+}
+
+object ZkConnect {
+  def parseString(connectString: String) = {
+    val parts = connectString.split("/", 2)
+    val servers = parts(0)
+    val chroot =
+      if (parts.size == 2) {
+        Some("/" + parts(1))
+      } else {
+        None
+      }
+    val hostAndPorts = servers.split(",").map {
+      server =>
+        val serverParts: Array[String] = server.split(":")
+        require(
+          serverParts.size <= 2,
+          s"wrong zk connect string format: multiple colons in host string \'$connectString\'"
+        )
+        val host = serverParts(0)
+        val port =
+          if (serverParts.size == 2) {
+            try {
+              Some(serverParts(1).toInt)
+            } catch {
+              case e: NumberFormatException =>
+                throw new IllegalArgumentException(s"bad port number in zk connect string: \'$connectString\'", e)
+            }
+          } else {
+            None
+          }
+        (host, port)
+    }
+    ZkConnect(hostAndPorts, chroot)
+  }
+}
